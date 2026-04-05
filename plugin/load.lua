6fbe0fcbabdc57d63c75b9bf670e313066c7b41b
Benjamin Collett <benjcollett@gmail.com>
2026-04-05 03:55:36 -0400

    optmized sending set buffer to avoid doing so on every key stroke, was required to do so for new mode to work in fuzz chips

--- plugin/load.lua
+++ plugin/load.lua
@@ -1,28 +1,18 @@
+local ipc = require("chipped")
+ipc.set_autoretry(true)
+ipc.connect()
+
+local last_insert_line = nil
+
 local function get_left_right_buffers()
 	local _, col = unpack(vim.api.nvim_win_get_cursor(0))
 	local line = vim.api.nvim_get_current_line()
 
-	-- Lua strings are 1-based
 	local left = line:sub(1, col)
 	local right = line:sub(col + 1)
 
 	return left, right
 end
-
-local ipc = require("chipped")
-ipc.set_autoretry(true)
-ipc.connect()
-
-vim.on_key(function()
-	local mode = vim.api.nvim_get_mode().mode
-	local m = mode:sub(1, 1)
-
-	if m == "i" or m == "c" or m == "t" then
-		return
-	end
-
-	ipc.send_clear_buffer()
-end, vim.api.nvim_create_namespace("ipc_cb"))
 
 local function update_buffers()
 	local left, right = get_left_right_buffers()
@@ -34,10 +24,37 @@ local group = vim.api.nvim_create_augroup("IPCInsertTracking", { clear = true })
 
 vim.api.nvim_create_autocmd("InsertEnter", {
 	group = group,
-	callback = update_buffers,
+	callback = function()
+		last_insert_line = vim.api.nvim_win_get_cursor(0)[1]
+		update_buffers()
+	end,
 })
 
 vim.api.nvim_create_autocmd("CursorMovedI", {
 	group = group,
-	callback = update_buffers,
+	callback = function()
+		local current_line = vim.api.nvim_win_get_cursor(0)[1]
+		if current_line ~= last_insert_line then
+			last_insert_line = current_line
+			update_buffers()
+		end
+	end,
 })
+
+vim.on_key(function(key)
+	local mode = vim.api.nvim_get_mode().mode
+	local m = mode:sub(1, 1)
+
+	if m == "i" then
+		if key == "<Del>" or key:match("^<[CMDA]%-") then
+			update_buffers()
+		end
+		return
+	end
+
+	if m == "c" or m == "t" then
+		return
+	end
+
+	ipc.send_clear_buffer()
+end, vim.api.nvim_create_namespace("ipc_cb"))
